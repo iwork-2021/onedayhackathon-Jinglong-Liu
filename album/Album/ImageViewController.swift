af63c58edd6a16eb48709caca402cf3aaafde065
Jinglong-Liu <1601029287@qq.com>
2021-12-21 19:26:14 +0800

    v1.0

---
+++ album/Album/ImageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageViewController.swift
+//  Album
+//
+//  Created by mac on 2021/12/22.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ album/Album/PicTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PicTableViewCell.swift
+//  Album
+//
+//  Created by mac on 2021/12/22.
+//
+
+import UIKit
+
+class PicTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageview: UIImageView!
+    
+    var img:UIImage = UIImage()
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
